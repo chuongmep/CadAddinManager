e78112e0c89bf931913f248020500c0e6f15176d
chuongmep <chuongpqvn@gmail.com>
2022-04-29 00:31:42 +0700

    Create LispCommandTest.cs

---
+++ Test/LispCommandTest.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.AutoCAD.ApplicationServices.Core;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
+namespace Test;
+
+public class LispCommand
+{
+    [CommandMethod("TestLispFunction", CommandFlags.Modal)]
+    public void Test()
+    {
+        ResultBuffer args = new ResultBuffer(
+            new TypedValue((int)LispDataType.Text, "c:hello"));
+        ResultBuffer result = Application.Invoke(args);
+        Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+        ed.WriteMessage(result.ToString());
+    }
+}
