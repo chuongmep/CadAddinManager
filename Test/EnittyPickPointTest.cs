a3eab2690d178a6e792fa0cd9d6358d35f90cf9c
Chuong Ho <31106432+chuongmep@users.noreply.github.com>
2022-04-28 14:56:11 +0700

    Create EnittyPickPointTest.cs

---
+++ Test/EnittyPickPointTest.cs
@@ -0,0 +1,30 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.ApplicationServices.Core;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+
+namespace Test;
+
+public class EntityPickPointTest
+{
+    [CommandMethod("getEntityPickPoint")]
+    public static void getEntityPickPoint()
+    {
+        Document doc = Application.DocumentManager.MdiActiveDocument;
+        Database db = doc.Database;
+        Editor ed = doc.Editor;
+
+        PromptEntityOptions peo = new PromptEntityOptions("\nSelect Entity");
+        PromptEntityResult per = ed.GetEntity(peo);
+        if (per.Status == PromptStatus.OK)
+        {
+            Point3d pickedPoint = per.PickedPoint;
+            Point3d pickedPtOsnap = ed.Snap("_near", pickedPoint);
+
+            ed.WriteMessage("\n Selected pick point: {0}", pickedPoint.ToString());
+            ed.WriteMessage("\n Selected Picked Point Osnap near: {0}", pickedPtOsnap);
+        }
+    }
+}
