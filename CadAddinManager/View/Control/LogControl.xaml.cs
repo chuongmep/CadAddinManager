a9eefc21c0058f3ebdf7f4edb3d01032e6eed9d2
chuongmep <chuongpqvn@gmail.com>
2022-05-16 23:53:05 +0700

    support copy content in debug/trace output

--- CadAddinManager/View/Control/LogControl.xaml.cs
+++ CadAddinManager/View/Control/LogControl.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows.Controls;
+﻿using System.Text;
+using System.Windows.Forms;
+using System.Windows.Input;
+using CadAddinManager.Model;
 using CadAddinManager.ViewModel;
+using UserControl = System.Windows.Controls.UserControl;
 
 namespace CadAddinManager.View.Control
 {
@@ -18,6 +22,18 @@ namespace CadAddinManager.View.Control
             this.Unloaded += viewModel.UserControl_Unloaded;
 
         }
-        
+         private void RightClickCopyCmdExecuted(object sender, ExecutedRoutedEventArgs e)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach (LogMessageString dd in listBox_LogMessages.SelectedItems)
+                    {
+                        sb.AppendLine(dd.Message);
+                    }
+                    Clipboard.SetText(sb.ToString());
+                }
+                private void RightClickCopyCmdCanExecute(object sender, CanExecuteRoutedEventArgs e)
+                {
+                    e.CanExecute = listBox_LogMessages.SelectedItem != null;
+                }
     }
 }
