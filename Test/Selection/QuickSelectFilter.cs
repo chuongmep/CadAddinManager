801ec22d962bbbf40585b32c4dbd10fce86cb9f6
chuongmep <chuongpqvn@gmail.com>
2022-05-29 23:18:04 +0700

    Create QuickSelectFilter.cs

---
+++ Test/Selection/QuickSelectFilter.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.AutoCAD.ApplicationServices.Core;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
+namespace Test.Selection;
+
+public class QuickSelectFilter
+{
+    [CommandMethod("SelectText")]
+    public static void SelectText()
+    {
+        var doc = Application.DocumentManager.MdiActiveDocument;
+        var db = doc.Database;
+        var ed = doc.Editor;
+        var peo = new PromptEntityOptions("\nSelect a text: ");
+        peo.SetRejectMessage("\nSelected object is not a text.");
+        peo.AddAllowedClass(typeof(DBText), true);
+        var per = ed.GetEntity(peo);
+        if (per.Status != PromptStatus.OK) 
+            return;
+        using (var tr = db.TransactionManager.StartTransaction())
+        {
+            var ent = (DBText)tr.GetObject(per.ObjectId, OpenMode.ForRead);
+            ed.WriteMessage($"\n{ent.TextString}");
+            tr.Commit();
+        }
+    }
+}
